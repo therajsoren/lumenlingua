4fe8f70f6db8cbd85ec7ca9f8055658e53ee0944
Raj Soren <7rajkumarsoren@gmail.com>
2025-07-13 03:59:23 +0530

    add preline configuration, add dot background

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      
+    <div className="absolute inset-0 h-full w-full bg-black bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]">
+      <div className="relative overflow-hidden h-screen">
+
+      </div>
     </div>
   );
 }
